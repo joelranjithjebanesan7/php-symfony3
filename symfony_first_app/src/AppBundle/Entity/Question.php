9a36b1d41c359347ae85950a5e5911b9775edbaf
root <joel@tivonaglobal.com>
2018-10-12 16:59:51 +0530

    Recreated repository commit

---
+++ symfony_first_app/src/AppBundle/Entity/Question.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Question
+ *
+ * @ORM\Table(name="question")
+ * @ORM\Entity
+ */
+
+class Question
+{
+/**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+*/
+    private $id;
+
+/**
+     * @var string
+     *
+     * @ORM\Column(name="question", type="string", length=545)
+*/
+    private $question;
+
+
+
+/**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set question
+     *
+     * @param string $question
+     *
+     * @return Question
+     */
+    public function setQuestion($question)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+}
